de48f3c4ed5e64c9135e09f7cf63e4842b03114c
Carolyn Webster-Pless <99274537+CarolynWP@users.noreply.github.com>
2022-03-22 14:47:57 +0000

    SGHSGHFGHSFGKFGHGHKGHKSGKHGHF FORGOT TO COMMIT IT AHJHSJKJDGJJDFGHDJGHKSGJSDFHGKSHDGKSHKGJFHDGDFGHDFGSHGHEHJDGDG also turns out I did a bunch of things wrong

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function doMathClicked () {
 	// Ominous message
-alert("I know you clicked the button. I know you didn't really do the math. You used Javascript for it, didn't you? You cheated. I don't want to do math with you anymore.")
+alert("Hey, HEY! Where did you find that? What is that? Okay, I'll let it slide for now, but DON'T CHEAT AGAIN!")
   // this function does math!!
-  document.getElementById('add-math').innerHTML = '75 + 43 =  ' + (75 + 43)
-  document.getElementById('subtract-math').innerHTML = '342 - 203 =  ' + (342 - 203)
-  document.getElementById('multiply-math').innerHTML = '87 + 103 * 333 =  ' + (87 + 103 * 333)
-  document.getElementById('divide-math').innerHTML = '(533 / 342) + 53 =  ' + ((533 / 342) + 53)
-  document.getElementById('exponent-math').innerHTML = '32 + 345 ** 3 =  ' + (32 + 345 ** 3)
-	document.getElementById('square-root-math').innerHTML = '√(327) = ' + Math.sqrt(327)
+  document.getElementById('add-math').innerHTML = 'The perimeter is ' + (1389 + 1389 + 3420 + 3420)
+}
+function doMathClicked2 () {
+	// Ominous message
+alert("I know you clicked that button. I know you didn't really do the math again. You used Javascript for it, didn't you? You cheated. Again. That's it, I don't want to do math with you anymore.")
+  // this function does math!!
+	  document.getElementById('multiply-math').innerHTML = 'The area is ' + (1389 * 3420)
 }
